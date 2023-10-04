01a551f4aff01814b03ff5f16c7534787582a440
Okosa Leonard <okosaleonel@gmail.com>
2023-10-04 12:32:24 -0700

    finished the video overlay and NavBar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,25 @@
 import cokevid from '../assets/cocacola vid.mp4'
+import img1 from "../assets/logo.svg"
+import Mvg from './Mvg'
+
 
 export default function NavBar() {
     return (
-        <div className="navba">
-            <div className="overlay"></div>
+        <div>
+             <div className="mainnav">
+                <img className='Logo' src={img1} />
+                <ul>
+                    <li><a href="">Brands</a></li>
+                    <li><a href="">Discover</a></li>
+                    <li><a href="">Impact</a></li>
+                    <li><a href="">Shop</a></li>
+                </ul>
+                <Mvg />
+            </div>
+            <div className="navba">
+                <div className="overlay"></div>
             <video src={cokevid} autoPlay loop muted />
+            </div>
         </div>
     )
  
---
+++ src/components/Mvg.tsx
@@ -0,0 +1,18 @@
+export default function Mvg(){
+    return(
+    <svg className="more" width="30px" height="30px" viewBox="0 0 24 24" fill="#fff" xmlns="http://www.w3.org/2000/svg">
+
+<title/>
+
+<g id="Complete">
+
+<g id="F-More">
+
+<path d="M8,12a2,2,0,1,1-2-2A2,2,0,0,1,8,12Zm10-2a2,2,0,1,0,2,2A2,2,0,0,0,18,10Zm-6,0a2,2,0,1,0,2,2A2,2,0,0,0,12,10Z" id="Horizontal"/>
+
+</g>
+
+</g>
+
+</svg>)
+}
